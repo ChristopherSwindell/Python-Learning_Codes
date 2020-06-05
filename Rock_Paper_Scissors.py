ec00414e57b825a6a0990854157052361055df44
ChristopherSwindell <66320159+ChristopherSwindell@users.noreply.github.com>
2020-06-05 09:44:42 -0500

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -3,44 +3,20 @@ import random
 out_list = []
 
 while True:
-    numgen=random.randint(0,3)
-    global opp_hand
+    hands = ['rock','paper','scissors']
+    opp_hand=random.choice(hands)
+    your_hand = input("Rock, Paper, or Scissors?     ")
     global outcome
-
-    if numgen == 1:
-        opp_hand = 'Rock'
-    elif numgen == 2:
-        opp_hand = 'Paper'
-    elif numgen == 3:
-        opp_hand = 'Scissors'
-
 
     if opp_hand == your_hand:
         outcome = 'tied'
-    elif opp_hand == 'Rock':
-        if your_hand == 'Paper':
-            outcome = 'lost'
-        elif your_hand == 'Scissors':
-            outcome = 'won'
-        elif your_hand not in ['rock', 'paper', 'scissors']:
-            outcome = 'lost'
-    elif opp_hand == "Paper":
-        if your_hand == "Rock":
-            outcome = 'lost'
-        elif your_hand == "Scissors":
-            outcome = 'won'
-        elif your_hand not in ['rock', 'paper', 'scissors']:
-            outcome = 'lost'
-    elif opp_hand == "Scissors":
-        if your_hand == "Rock":
-            outcome = 'won'
-        elif your_hand == "Papaer":
-            outcome = 'lost'
-        elif your_hand not in ['rock', 'paper', 'scissors']:
-            outcome = 'lost'
+    elif opp_hand+your_hand in ['rockpaper','paperscissors','scissorsrock']:
+        outcome = 'won'
+    else:
+        outcome = 'lost'
 
     out_list.append(outcome)
-    your_hand = input("Rock, Paper, or Scissors?     ")
+
     print("Your opponent chose", opp_hand,".")
     print("You", outcome,".")
     print("You have won",out_list.count('won'), "out of",len(out_list),"games.")
